2ef6f81011eb770f718be5930077c940eca88f55
LONTSI TCHOUPOU Nathanaël Mesmer <tchopounathanael@gmail.com>
2022-12-20 10:50:26 +0100

    add a for loop

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,4 +1,4 @@
-let bttn = document.querySelector('bttn')
+let button = document.querySelectorAll('.bttn')
 let btn1 = document.getElementById('1')
 let btn2 = document.getElementById('2')
 let btn3 = document.getElementById('3')
@@ -10,28 +10,29 @@ let btn8 = document.getElementById('8')
 let btn9 = document.getElementById('9')
 let array = [1, 2, 3, 6, 9, 8, 7, 4]
 
-btn5.onclick = function clockwise1() {
-    clockwise()
-    paint()
+btn5.onclick = function() {
+  clockwise()
+  swapp()
 }
-
-bttn.onclick = function anticlockwise1() {
-  anticlockwise()
-  paint()
+for (const i of button) {
+  i.onclick = function anti1() {
+    anticlockwise()
+    swapp()
+  }
 }
 
 
 function clockwise() {
-  btn5 = array.pop()
+  let   btn5 = array.pop()
   array.unshift(btn5)
 }
 
 function anticlockwise() {
-  bttn = array.unshift()
-  array.push(bttn)
+  let swapp = array.shift()
+  array.push(swapp)
 }
 
-function paint() {
+function swapp() {
   btn1.innerHTML = array[0]
   btn2.innerHTML = array[1]
   btn3.innerHTML = array[2]
@@ -41,14 +42,3 @@ function paint() {
   btn8.innerHTML = array[5]
   btn9.innerHTML = array[4]
 }
-
-/* function swapp() {
-  btn1.innerHTML = arr[0]
-  btn2.innerHTML = arr[7]
-  btn3.innerHTML = arr[6]
-  btn4.innerHTML = arr[1]
-  btn6.innerHTML = arr[5]
-  btn7.innerHTML = arr[2]
-  btn8.innerHTML = arr[3]
-  btn9.innerHTML = arr[4]
-} */
