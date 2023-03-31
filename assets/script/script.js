4fd2f15e4943c42b7a6ca9b027e342fd5473a562
LONTSI TCHOUPOU Nathanaël Mesmer <tchopounathanael@gmail.com>
2023-03-31 13:30:27 +0100

    done with styles

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,43 +1,21 @@
-const button = document.querySelectorAll('.bttn')
-const btn1 = document.getElementById('1')
-const btn2 = document.getElementById('2')
-const btn3 = document.getElementById('3')
-const btn4 = document.getElementById('4')
-const btn5 = document.getElementById('5')
-const btn6 = document.getElementById('6')
-const btn7 = document.getElementById('7')
-const btn8 = document.getElementById('8')
-const btn9 = document.getElementById('9')
-const array = [1, 2, 3, 6, 9, 8, 7, 4]
+const start = document.getElementById("AC");
+const plusminus = document.getElementById("+-");
+const pourcentage = document.getElementById("%");
+const division = document.getElementById("/");
+const seven = document.getElementById("7");
+const eight = document.getElementById("8");
+const nine = document.getElementById("9");
+const multiplication = document.getElementById("*");
+const four = document.getElementById("4");
+const five = document.getElementById("5");
+const six = document.getElementById("6");
+const subtraction = document.getElementById("-");
+const one = document.getElementById("1");
+const two = document.getElementById("2");
+const three = document.getElementById("3");
+const addition = document.getElementById("+");
+const zero = document.getElementById("0");
+const point = document.getElementById(".");
+const equality = document.getElementById("=");
 
-btn5.onclick = function () {
-  clockwise()
-  swapp()
-}
-for (const i of button) {
-  i.onclick = function () {
-    anticlockwise()
-    swapp()
-  }
-}
-
-function clockwise () {
-  const btn5 = array.pop()
-  array.unshift(btn5)
-}
-
-function anticlockwise () {
-  const swapp = array.shift()
-  array.push(swapp)
-}
-
-function swapp () {
-  btn1.innerHTML = array[0]
-  btn2.innerHTML = array[1]
-  btn3.innerHTML = array[2]
-  btn4.innerHTML = array[7]
-  btn6.innerHTML = array[3]
-  btn7.innerHTML = array[6]
-  btn8.innerHTML = array[5]
-  btn9.innerHTML = array[4]
-}
+// andela personal evaluation form
